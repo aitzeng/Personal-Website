9afed21e6bcaadd7229c6976071ac26fe928bff7
aitzeng <aitzeng@gmail.com>
2023-10-05 01:42:08 -0700

    Added LinkedIn and github links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 
 function NavBar() {
   return (
     <header>
       <nav className="bg-slate-950 md:flex md:items-center md:justify-between" >
-        <div>
-          <a className='mx-4 text-slate-50 text-2xl' href="/home">Anthony Tzeng</a>
+        <div className="mx-4 md:flex text-slate-50 text-2xl">
+          <a className='mx-4' href="https://www.linkedin.com/in/anthonytzeng/"><AiFillLinkedin/></a>
+          <a className='mx-4' href="https://github.com/aitzeng"><AiFillGithub/></a>
         </div>
         <ul>
           <a className='mx-4 text-slate-50 text-2xl' href="/home">Home</a>
