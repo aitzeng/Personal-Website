650e1219fead6eba6b6bf255ae807fa6ffc2733a
aitzeng <aitzeng@gmail.com>
2023-10-05 18:06:44 -0700

    Added links to portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Portfolio() {
   return (
-    <div>
-      <div>This is the Portfolio Page</div>
+    <div className="bg-slate-950 h-screen w-screen flex flex-col">
+      <div className="main">
+        <h3 className="text-slate-50 text-6xl mx-4 my-4">Portfolio</h3>
+        <div className="text-slate-50 flex w-1/2">
+          <a className="mx-4 my-4 border-red-500 border-2 rounded flex flex-col justify-center w-1/5" href="https://github.com/aitzeng/wuphf">
+          <span>WUPHF</span><span>Application to send alerts using various communication methods</span></a>
+          <a className="mx-4 my-4 border-red-500 border-2 rounded flex flex-col justify-center w-1/5" href="https://github.com/aitzeng/personal-website">This website!</a>
+          <a className="mx-4 my-4 border-red-500 border-2 rounded flex flex-col justify-center w-1/5" href="https://github.com/JoJos-Blue-Ocean/TheWorld">The World</a>
+          <a className="mx-4 my-4 border-red-500 border-2 rounded flex flex-col justify-center w-1/5" href="https://github.com/CBC-Ecommerce/styl-forge">Styl Forge</a>
+          <a className="mx-4 my-4 border-red-500 border-2 rounded flex flex-col justify-center w-1/5" href="https://github.com/luminescent-linguica">Atelier System Design</a>
+        </div>
+      </div>
     </div>
   )
 }
