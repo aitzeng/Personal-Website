e23f92499d88220c7ac9305d80860f033e75408e
aitzeng <aitzeng@gmail.com>
2023-10-05 21:39:17 -0700

    Updated About text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,8 +7,8 @@ function About() {
       <div className="content flex-col">
         <h3 className="text-slate-50 text-6xl mx-4 my-4 underline">About Me</h3>
         <div className="flex mx-4 my-4">
-          <div className='flex-col w-3/4 text-slate-50 text-xl mr-10'>
-            <p>My name is Anthony and one of my hobbies is playing competitive video games! Most recently, I delved into the world of Valorant, where I achieved Immortal 2 and reached the top 0.2% of all players!  Besides video games, I enjoy spending my free time helping the community! I've volunteered as a swim coach in high school. In college, I volunteered at an animal shelter and as a columnist for the school newspaper! Prior to COVID, I spent my free time as an AYSO coach and led the team to win every regular season game!
+          <div className='flex-col w-2/3 text-slate-50 text-xl mr-10'>
+            <p>If you've clicked this far, then my website has done its job! One of my hobbies is swimming! I've been doing it ever since I was 6 and have been fortunate enough to continue it in my free time. I've swam competitively throughout my childhood and in 2021,I took it to the next level by participating in the 6-mile Bridge to Bridge swim in San Francisco! This past summer, I also went scuba diving in Puerto Rico and saw the most amazing sea creatures. Besides swimming, I enjoy spending my free time helping the community! I've volunteered as a swim coach in high school. In college, I volunteered at an animal shelter and as a columnist for the school newspaper! Prior to COVID, I spent my free time as an AYSO coach and led the youth team to win every regular season game!
             </p>
             <br/>
             <p>I am a software engineer with a previous background in biotech clinical operations and a software engineering certificate from Hack Reactor. My experience at Hack Reactor has taught me to work well in an Agile team, read code, and problem solve in all stages of the software engineering lifecycle. My time at FivePrime/Amgen has allowed me to enhance my collaboration and organizational skills, and I have demonstartated my ability to successfully launch global programs.
