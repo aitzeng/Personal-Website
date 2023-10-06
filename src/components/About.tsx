58c955d70f7274673341ec6137a3c21be7ddc690
aitzeng <aitzeng@gmail.com>
2023-10-05 19:53:12 -0700

    Removed about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,6 @@ function About() {
     <div>
       <div>About Section</div>
       <p>Full Stack Software Engineer</p>
-      <p>About a year or so ago while working at Amgen, I took an interest in coding after tweaking around with some excell spreadsheets. Cut to today with a few projects under my belt and completing an advanced software engineering immersive course from Hack Reactor, I'm proud to call myself a software engineer! I'm passionate about building applications that help people's everyday or challenge me to expand my knowledge and understanding. </p>
     </div>
   )
 }
