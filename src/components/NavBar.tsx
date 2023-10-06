c85bb85303c60d2741e97e89545cb9c3c72f0f98
aitzeng <aitzeng@gmail.com>
2023-10-05 20:11:30 -0700

    Added email contact

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from 'react-icons/ai';
 import resume from '../assets/Resume.pdf';
 
 function NavBar() {
@@ -9,13 +9,15 @@ function NavBar() {
         <div className="mx-4 md:flex text-slate-50 text-3xl">
           <a className='mx-4 hover:scale-110 transition-transform' target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/anthonytzeng/"><AiFillLinkedin/></a>
           <a className='mx-4 hover:scale-110 transition-transform' target="_blank" rel="noopener noreferrer" href="https://github.com/aitzeng"><AiFillGithub/></a>
+          <a className='mx-4 hover:scale-110 transition-transform' href="mailto:aitzeng@gmail.com"><AiOutlineMail/></a>
+
         </div>
         <ul>
           <a className='mx-4 text-slate-50 text-2xl hover:underline' href="/home">Home</a>
           <a className='mx-4 text-slate-50 text-2xl hover:underline' href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
           {/* <a className='mx-4 text-slate-50 text-2xl hover:underline' href="/about">About</a> */}
           <a className='mx-4 text-slate-50 text-2xl hover:underline' href="/portfolio">Portfolio</a>
-          <a className='mx-4 text-slate-50 text-2xl hover:underline' href="/contact">Contact</a>
+          {/* <a className='mx-4 text-slate-50 text-2xl hover:underline' href="/contact">Contact</a> */}
         </ul>
       </nav>
     </header>
